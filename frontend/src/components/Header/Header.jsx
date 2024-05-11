7253e5e22673e4e04ebbf1284f9f73b63a2812fa
Pintu kumar <pintu1012kumar@gmail.com>
2024-05-11 18:09:57 +0530

    second

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Header.css'
+
+const Header =() => {
+    return (
+        <div className="header">
+         <div className="header-contents">
+            <h2>Order your favourite food here..</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem vel praesentium quo ratione velit illo natus est quibusdam unde architecto voluptatum, quisquam harum enim reprehenderit optio explicabo ex dolore soluta.</p>
+            <button>View Menu</button>
+         </div>
+        </div>
+    )
+}
+
+export default Header
